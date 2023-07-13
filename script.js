8325ec2e00aa55e44dc4252242b42bf79d404e15
{marimo_7} <{220098tt@yse-c.net}>
2023-07-13 12:33:43 +0900

    up

--- script.js
+++ script.js
@@ -1,123 +1,125 @@
-const minPartConfidence = 0.2; // キーポイントの最小信頼度のしきい値。この値以下の信頼度のキーポイントは描画されません。
-const color1 = 'aqua'; // ビデオ1で描画するキーポイントとスケルトンの色。
-const color2 = 'red'; // ビデオ2で描画するキーポイントとスケルトンの色。
-const lineWidth = 3; // スケルトンの線の太さ。
-const maxAllowError = 50; // 許容される最大の角度誤差。ポーズの比較において、この値以下の角度誤差は「GOOD!」と判定されます。
+const minPartConfidence = 0.2; // 最小の信頼度のしきい値
+const color1 = 'aqua'; // video1 のキーポイントとスケルトンの色
+const color2 = 'red'; // video2 のキーポイントとスケルトンの色
+const lineWidth = 3; // スケルトンの線の太さ
+const maxAllowError = 50; // 許容される最大のエラー
 
-// 以下の変数は、HTML要素から取得するためのコードが省略されているものとします
-const video1 = document.getElementById('video1'); // ビデオ1のHTML要素。解析に使用するビデオが設定されています。
-const canvas1 = document.getElementById('canvas1'); // キャンバス1のHTML要素。ビデオ1の骨格表示のために使用されます。
-const contentWidth1 = canvas1.width; // キャンバス1の幅。
-const contentHeight1 = canvas1.height; // キャンバス1の高さ。
-const ctx1 = canvas1.getContext('2d'); // キャンバス1のコンテキスト。描画操作に使用されます。
-const video2 = document.getElementById('video2'); // ビデオ2のHTML要素。ウェブカメラからの映像が設定されています。
-const canvas2 = document.getElementById('canvas2'); // キャンバス2のHTML要素。ビデオ2の骨格表示のために使用されます。
-const contentWidth2 = canvas2.width; // キャンバス2の幅。
-const contentHeight2 = canvas2.height; // キャンバス2の高さ。
-const ctx2 = canvas2.getContext('2d'); // キャンバス2のコンテキスト。描画操作に使用されます。
+// video1 の要素と canvas1 の要素を取得
+const video1 = document.getElementById('video1');
+const canvas1 = document.getElementById('canvas1');
+const contentWidth1 = canvas1.width;
+const contentHeight1 = canvas1.height;
+const ctx1 = canvas1.getContext('2d');
 
-let correct_pose; // 正しいポーズの情報を格納する変数。ループ内で更新されます。
-let user_pose; // ユーザのポーズの情報を格納する変数。ループ内で更新されます。
-let error; // ポーズの角度誤差を格納する変数。ループ内で計算されます。
-let intervalId; // setIntervalメソッドのインターバルIDを格納する変数。ループの停止に使用されます。
-let score = 0; // スコアを格納する変数。正解したポーズの数をカウントします。初期値は0です。
+// video2 の要素と canvas2 の要素を取得
+const video2 = document.getElementById('video2');
+const canvas2 = document.getElementById('canvas2');
+const contentWidth2 = canvas2.width;
+const contentHeight2 = canvas2.height;
+const ctx2 = canvas2.getContext('2d');
 
-//ウェブカメラ作動
-navigator.getUserMedia(
-  { video: {} },
-  stream => video2.srcObject = stream,
-  err => console.error(err)
-)
+let correct_pose; // 正しいポーズ
+let user_pose; // ユーザーのポーズ
+let error; // エラー
+let intervalId; // setInterval の ID
+let score = 0; // スコア
 
-//ボタンをクリックするとスタート（ビデオが流れる）
-document.getElementById("start-button").onclick = function () {
-  target_score = document.getElementById("score");
-  target_score.innerHTML = "SCORE: " + String(score);
-  target = document.getElementById("good");
-  target.innerHTML = "　";
-  video1.play();
+navigator.mediaDevices.getUserMedia({ video: true }) // getUserMedia を使ってカメラのストリームを取得
+  .then(stream => {
+    video2.srcObject = stream;
+    video2.onloadedmetadata = () => {
+      resizeCanvas(video2, canvas2);
+      startVideo();
+    };
+  })
+  .catch(err => console.error(err));
+
+function resizeCanvas(video, canvas) {
+    canvas.width = video.videoWidth; // キャンバスの幅を動画の幅に合わせる
+    canvas.height = video.videoHeight; // キャンバスの高さを動画の高さに合わせる
+  }
+  
+document.getElementById('start-button').onclick = function () { // スタートボタンがクリックされたときの処理
+  target_score = document.getElementById('score');
+  target_score.innerHTML = 'SCORE: ' + String(score);
+  target = document.getElementById('good');
+  target.innerHTML = '　';
+  video1.play(); // video1 を再生
 };
-
-//ボタンをクリックするとストップ
-document.getElementById("stop-button").onclick = function () {
+  
+document.getElementById('stop-button').onclick = function () { // ストップボタンがクリックされたときの処理
   stopLoop();
 };
 
-// 追加
-function startVideo() {
-  video1.play();
-}
-
-
-// ビデオが再生される際のイベントリスナー
-video1.addEventListener('play', () => {
-  intervalId = setInterval(async () => {
-    // ビデオ1の骨格表示（Multiple複数)
-    posenet.load().then(function (net) {
-      return net.estimateMultiplePoses(video1, {
-        flipHorizontal: false,
-        maxDetections: 2,
-        scoreThreshold: 0.6,
-        nmsRadius: 20
-      });
-    }).then(function (poses) {
-      console.log("左側", poses);
-      ctx1.clearRect(0, 0, contentWidth1, contentHeight1);
-      poses.forEach(({ keypoints }) => {
-        drawKeypoints(keypoints, minPartConfidence, ctx1, color1);
-        drawSkeleton(keypoints, minPartConfidence, ctx1, color1);
-      });
-
-      correct_pose = poses[0];
+function startVideo() { // 動画の処理を開始する関数
+  video1.addEventListener('play', () => { // video1 が再生されたときのイベントリスナー
+    window.addEventListener('resize', () => {
+      resizeCanvasToWindowSize(canvas1);
+      resizeCanvasToWindowSize(canvas2);
     });
+    
+    intervalId = setInterval(() => {
+      posenet.load().then(net => { // posenet.load() でモデルをロード
+        return net.estimateMultiplePoses(video1, {
+          flipHorizontal: false,
+          maxDetections: 2,
+          scoreThreshold: 0.6,
+          nmsRadius: 20,
+        });
+      }).then(poses => { // ポーズを推定
+        ctx1.clearRect(0, 0, contentWidth1, contentHeight1);
+        poses.forEach(({ keypoints }) => {
+          drawKeypoints(keypoints, minPartConfidence, ctx1, color1);
+          drawSkeleton(keypoints, minPartConfidence, ctx1, color1);
+        });
 
-    // ウェブカメラの骨格表示（Single 1人)
-    posenet.load().then(function (net) {
-      const pose = net.estimateSinglePose(video2, {
-        flipHorizontal: true
+        correct_pose = poses[0]; // 正しいポーズは配列の先頭にあると仮定
+        if (user_pose) {
+          error = calcAngleError(correct_pose, user_pose); // エラーを計算
+          target = document.getElementById('good');
+
+          if (error <= maxAllowError) {
+            target.innerHTML = 'GOOD!';
+            score = score + 1;
+            target_score = document.getElementById('score');
+            target_score.innerHTML = 'SCORE: ' + String(score);
+          } else {
+            target.innerHTML = '　';
+          }
+        }
       });
-      return pose;
-    }).then(function (pose) {
-      // console.log("右側",pose);
-      ctx2.clearRect(0, 0, contentWidth2, contentHeight2);
-      drawKeypoints(pose.keypoints, minPartConfidence, ctx2, color2);
-      drawSkeleton(pose.keypoints, minPartConfidence, ctx2, color2);
-      user_pose = pose;
-    });
+    }, 500);
+  });
 
-    error = calcAngleError(correct_pose, user_pose);
-    target = document.getElementById("good");
-
-    if (error <= maxAllowError) {
-      target.innerHTML = "GOOD!";
-      score = score + 1;
-      target_score = document.getElementById("score");
-      target_score.innerHTML = "SCORE: " + String(score);
-    } else {
-      target.innerHTML = "　";
-    }
-  }, 500);
-});
-
-// ループを停止する関数
-function stopLoop() {
+  posenet.load().then(net => { // posenet.load() でモデルをロード
+    setInterval(() => {
+      net.estimateSinglePose(video2, {
+        flipHorizontal: true,
+      }).then(pose => { // カメラからのポーズ推定
+        ctx2.clearRect(0, 0, contentWidth2, contentHeight2);
+        drawKeypoints(pose.keypoints, minPartConfidence, ctx2, color2);
+        drawSkeleton(pose.keypoints, minPartConfidence, ctx2, color2);
+        user_pose = pose; // ユーザーのポーズを設定
+      });
+    }, 500);
+  });
+  
+function stopLoop() { // ループを停止する関数
   clearInterval(intervalId);
-  video1.pause(); // 動画の再生を停止する
+  video1.pause();
 }
-
-// Keypointを座標のタプルに変換する関数
+  
 function toTuple({ y, x }) {
   return [y, x];
 }
 
-// キーポイントを描画する関数
-function drawKeypoints(keypoints, minConfidence, ctx, color, scale = 1) {
+function drawKeypoints(keypoints, minConfidence, ctx, color, scale = 1) { // キーポイントを描画する関数
   const excludedParts = ['leftEye', 'rightEye', 'leftEar', 'rightEar'];
 
   for (let i = 0; i < keypoints.length; i++) {
     const keypoint = keypoints[i];
     if (keypoint.score < minConfidence || excludedParts.includes(keypoint.part)) {
-      continue; // 信頼度が低い場合または頭部のキーポイントの場合は描画しない
+      continue;
     }
     const { y, x } = keypoint.position;
     drawPoint(ctx, y * scale, x * scale, 3, color);
@@ -131,8 +133,6 @@ function drawPoint(ctx, y, x, r, color) {
   ctx.fill();
 }
 
-
-// セグメントを描画する関数
 function drawSegment([ay, ax], [by, bx], color, scale, ctx) {
   ctx.beginPath();
   ctx.moveTo(ax * scale, ay * scale);
@@ -142,34 +142,29 @@ function drawSegment([ay, ax], [by, bx], color, scale, ctx) {
   ctx.stroke();
 }
 
-// スケルトンを描画する関数
 function drawSkeleton(keypoints, minConfidence, ctx, color, scale = 1) {
   const adjacentKeyPoints = posenet.getAdjacentKeyPoints(keypoints, minConfidence);
 
-  adjacentKeyPoints.forEach((keypoints) => {
+  adjacentKeyPoints.forEach(adjacentKeypoints => {
     drawSegment(
-      toTuple(keypoints[0].position), toTuple(keypoints[1].position), color,
-      scale, ctx);
+      toTuple(adjacentKeypoints[0].position),
+      toTuple(adjacentKeypoints[1].position),
+      color,
+      scale,
+      ctx
+    );
   });
 }
 
-// ループ内での角度誤差を計算する関数
 function calcAngleError(correct_pose, user_pose) {
   let error = 0;
 
-  // Shoulder - Elbow
   error += calcKeypointAngleError(correct_pose, user_pose, 5, 7);
   error += calcKeypointAngleError(correct_pose, user_pose, 6, 8);
-
-  // Elbow - Wrist
   error += calcKeypointAngleError(correct_pose, user_pose, 7, 9);
   error += calcKeypointAngleError(correct_pose, user_pose, 8, 10);
-
-  // Hip - Knee
   error += calcKeypointAngleError(correct_pose, user_pose, 11, 13);
   error += calcKeypointAngleError(correct_pose, user_pose, 12, 14);
-
-  // Knee - Ankle
   error += calcKeypointAngleError(correct_pose, user_pose, 13, 15);
   error += calcKeypointAngleError(correct_pose, user_pose, 14, 16);
 
@@ -178,9 +173,11 @@ function calcAngleError(correct_pose, user_pose) {
   return error;
 }
 
-// 正解ポーズとユーザポーズの、ある2つのキーポイント間の角度の誤差を計算
 function calcKeypointAngleError(correct_pose, user_pose, num1, num2) {
-  let error = Math.abs(calcKeypointsAngle(correct_pose.keypoints, num1, num2) - calcKeypointsAngle(user_pose.keypoints, num1, num2))
+  let error = Math.abs(
+    calcKeypointsAngle(correct_pose.keypoints, num1, num2) -
+    calcKeypointsAngle(user_pose.keypoints, num1, num2)
+  );
   if (error <= 180) {
     return error;
   } else {
@@ -188,17 +185,36 @@ function calcKeypointAngleError(correct_pose, user_pose, num1, num2) {
   }
 }
 
-// キーポイント[num1]とキーポイント[num2]の角度を計算
 function calcKeypointsAngle(keypoints, num1, num2) {
-  return calcPositionAngle(keypoints[num1].position, keypoints[num2].position);
+  return calcPositionAngle(
+    keypoints[num1].position,
+    keypoints[num2].position
+  );
 }
 
-// position1とposition2を結ぶ線分の角度を計算
 function calcPositionAngle(position1, position2) {
   return calcAngleDegrees(position1.x, position1.y, position2.x, position2.y);
 }
 
-// 2点間の角度を計算
 function calcAngleDegrees(x1, y1, x2, y2) {
   return Math.atan2(y2 - y1, x2 - x1) * 180 / Math.PI;
+}
+
+function resizeCanvasToVideoSize(video, canvas) {
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+}
+
+function resizeCanvasToWindowSize(canvas) {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+
+function resizeCanvas(video, canvas) {
+  if (window.innerWidth > video.videoWidth) {
+    resizeCanvasToVideoSize(video, canvas);
+  } else {
+    resizeCanvasToWindowSize(canvas);
+  }
+}
 }
